1bb2a7ed1e6cc70bb2ac3cb89c7f2b589b76acfd
sheeluofficial <sheeluofficial@gmail.com>
2023-04-29 09:43:34 +0530

    B25 NXM201 S4 PSC added mostsearchedcity and my City route

--- BackEnd/Assignments/NXM201_S4_PSC/NXM201_C4_B24/wheather_app_evaluation/controllers/cities.controller.js
+++ BackEnd/Assignments/NXM201_S4_PSC/NXM201_C4_B24/wheather_app_evaluation/controllers/cities.controller.js
@@ -5,29 +5,105 @@ const { UserCitiesList } = require("../models/city.model");
 const API_KEY = process.env.OW_API_KEY;
 
 
-const getCityData =  async (req, res) => {
-    try {
-      const city = req.params.city;
-      const isCityInCache = await redisClientisClient.get(city);
-      // If the city is already in cache
-      if (isCityInCache) return res.status(200).send(isCityInCache);
-      const response = await axios.get(
-        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
-      );
-      const weatherData = response.data;
-      // Store the weathe data in the cache and set expirty time for 30 min ( 30 * 60sec)
-      await redisClient.set(city, JSON.stringify(weatherData), { EX: 30 * 60 });
-  
-  
-       await UserCitiesList.findOneAndUpdate({userId:req.body.userId},{userId: req.body.userId, $push: { previousSearches: city } 
-      },{ upsert: true, new: true, setDefaultsOnInsert: true })
-  
-      return res.status(200).send({ data: weatherData });
-    } catch (error) {
-      // using logger to log the errors to mongodb errors collection
-      logger.error(error);
-      return res.status(500).send({ error: error.message });
+const getCityData = async (req, res) => {
+  try {
+    const city = req.params.city || req.preferred_city;
+
+    const isCityInCache = await redisClientisClient.get(city);
+    // If the city is already in cache
+    if (isCityInCache) return res.status(200).send(isCityInCache);
+    const response = await axios.get(
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+    );
+    const weatherData = response.data;
+    // Store the weathe data in the cache and set expirty time for 30 min ( 30 * 60sec)
+    await redisClient.set(city, JSON.stringify(weatherData), { EX: 30 * 60 });
+
+
+    await UserCitiesList.findOneAndUpdate({ userId: req.body.userId }, {
+      userId: req.body.userId, $push: { previousSearches: city }
+    }, { upsert: true, new: true, setDefaultsOnInsert: true })
+
+    return res.status(200).send({ data: weatherData });
+  } catch (error) {
+    // using logger to log the errors to mongodb errors collection
+    logger.error(error);
+    return res.status(500).send({ error: error.message });
+  }
+}
+
+
+const mostSearchedCity = async (req, res, next) => {
+
+
+  try {
+    const city = await UserCitiesList.aggregate([{ $match: { userId: req.userId } }, { $unwind: "$previousSearches" }, {
+      $group: {
+        _id: "$previousSearches", "count": { "$sum": 1 }
+      }
+    }, 
+    {
+      $sort:{count:-1}
     }
+    ]);
+
+
+    const isCityInCache = await redisClientisClient.get(city);
+
+    // If the city is already in cache
+    if (isCityInCache) return res.status(200).send(isCityInCache);
+
+    const response = await axios.get(
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+    );
+
+    const weatherData = response.data;
+    // Store the weathe data in the cache and set expirty time for 30 min ( 30 * 60sec)
+    await redisClient.set(city, JSON.stringify(weatherData), { EX: 30 * 60 });
+
+
+    await UserCitiesList.findOneAndUpdate({ userId: req.body.userId }, {
+      userId: req.body.userId, $push: { previousSearches: city }
+    }, { upsert: true, new: true, setDefaultsOnInsert: true })
+
+    return res.status(200).send({ data: weatherData });
+    
+  } catch (error) {
+    // using logger to log the errors to mongodb errors collection
+    logger.error(error);
+    return res.status(500).send({ error: error.message });
   }
 
-  module.exports = getCityData
+}
+
+const myCity = async (req, res, next) => {
+
+  try {
+    
+    const city = req.preferred_city;
+
+
+    const isCityInCache = await redisClientisClient.get(city);
+
+    // If the city is already in cache
+    if (isCityInCache) return res.status(200).send(isCityInCache);
+
+    const response = await axios.get(
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+    );
+
+    const weatherData = response.data;
+    // Store the weathe data in the cache and set expirty time for 30 min ( 30 * 60sec)
+    await redisClient.set(city, JSON.stringify(weatherData), { EX: 30 * 60 });
+
+    return res.status(200).send({ data: weatherData });
+
+  } catch (error) {
+    // using logger to log the errors to mongodb errors collection
+    logger.error(error);
+    return res.status(500).send({ error: error.message });
+  }
+
+}
+
+module.exports = {getCityData, myCity, mostSearchedCity}
--- BackEnd/Assignments/NXM201_S4_PSC/NXM201_C4_B24/wheather_app_evaluation/routes/cities.routes.js
+++ BackEnd/Assignments/NXM201_S4_PSC/NXM201_C4_B24/wheather_app_evaluation/routes/cities.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const cityRouter = Router();
 const { cityValidator } = require("../middlewares/cityvalidator");
+const { getCityData, mostSearchedCity, myCity } = require("../controllers/cities.controller");
 
-const getCityData = require("../controllers/cities.controller");
+
 
 
 cityRouter.get("/:city", cityValidator, getCityData);
+cityRouter.get("/mostsearchedcity", mostSearchedCity);
+cityRouter.get("/mycity", myCity);
 
 module.exports = { cityRouter };
